8f86db39e34481660c18ddf95c8d73606cbeedbe
giansteve <gianmarcomelito@gmail.com>
2021-12-06 17:57:42 +0100

    improved graphic output of run_model.m and added name of figure output

--- Cardiovascular-Systems-Model-PH/Code/run_model.m
+++ Cardiovascular-Systems-Model-PH/Code/run_model.m
@@ -3,10 +3,17 @@
 % This driver file will call each patient's optimized parameters and plot
 % the model predictions compared to the static and dynamics data.
 
+% set graphic preference
+set(0,'DefaultFigureWindowStyle','default')
+set(groot, 'defaultAxesTickLabelInterpreter','latex');
+set(groot, 'defaultLegendInterpreter','latex');
+set(0,'DefaultTextInterpreter','latex')
+set(0,'defaultAxesFontSize',11)
+
 %
 
 %% Call CV model and plot optimial prediction
 clear; clc;
-PATIENT = 1;  %% Change for each patient (1-5), or use 6 for normotensive
+PATIENT = 5;  %% Change for each patient (1-5), or use 6 for normotensive
 RESIDUAL = 2; %% Change for each residual (1-2)
 [p,V,q] = Driver_postopt(PATIENT,RESIDUAL); % Returns pressure, volume, flow
